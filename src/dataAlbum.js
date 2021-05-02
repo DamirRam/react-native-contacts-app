c549bcd4f482f91f26f25565ec4a63098d24c3fe
unknown <d.a.ramazanov@gmail.com>
2021-05-02 15:50:11 +0300

    crlf to lf

--- src/dataAlbum.js
+++ src/dataAlbum.js
@@ -1,18 +1,18 @@
 export const dataAlbum = [
-    {
-      title: 'Album One',
-      value: 'https://jsonplaceholder.typicode.com/albums/1/photos?_limit=20'
-    },
-    {
-      title: 'Album Two',
-      value: 'https://jsonplaceholder.typicode.com/albums/2/photos?_limit=20'
-    },
-    {
-      title: 'Album Three',
-      value: 'https://jsonplaceholder.typicode.com/albums/3/photos?_limit=20'
-    },
-    {
-      title: 'Album Four',
-      value: 'https://jsonplaceholder.typicode.com/albums/4/photos?_limit=20'
-    }
-  ];
+  {
+    title: 'Album One',
+    value: 'https://jsonplaceholder.typicode.com/albums/1/photos?_limit=20',
+  },
+  {
+    title: 'Album Two',
+    value: 'https://jsonplaceholder.typicode.com/albums/2/photos?_limit=20',
+  },
+  {
+    title: 'Album Three',
+    value: 'https://jsonplaceholder.typicode.com/albums/3/photos?_limit=20',
+  },
+  {
+    title: 'Album Four',
+    value: 'https://jsonplaceholder.typicode.com/albums/4/photos?_limit=20',
+  },
+];
